7273ad9bb3e91580034de2915935390aa4d50dbc
rh <ryuta_sp_aimi@yahoo.co.jp>
2019-04-05 15:11:55 +0900

    refactoring

--- github_open_compare_diff.js
+++ github_open_compare_diff.js
@@ -1,30 +1,60 @@
 javascript:(() => {
-  document.querySelector('html').onclick = (e) => {
+  if (!isActiveCompare()) setEvent();
+
+  /* ------------------------ 以下関数群 ------------------------ */
+  function isActiveCompare() {
+    return document.querySelector('html').dataset.isActiveCompare === 'true';
+  }
+  function setEvent() {
+    document.querySelector('html').addEventListener('click', main);
+    document.querySelector('html').dataset.isActiveCompare = true
+  }
+  function removeEvent() {
+    document.querySelector('html').removeEventListener('click', main);
+    document.querySelector('html').dataset.isActiveCompare = false
+  }
+  function main(event) {
+    event.preventDefault();
+    const el = event.target;
+
     /* コミットのリンクの場合 */
-    if (e.target.classList.contains('commit-id')) {
+    if (isCommitLink(el)) {
       /* 選択状態にしたり外したり */
-      if (e.target.dataset.isDiffSelected === 'true') {
-        e.target.style['background-color'] = '';
-        e.target.dataset.isDiffSelected = false;
+      if (el.dataset.isDiffSelected === 'true') {
+        unselectCommit(el)
       } else {
-        e.target.dataset.isDiffSelected = true;
-        e.target.style['background-color'] = '#404040';
+        selectCommit(el);
       }
 
-      const selectedNode = document.querySelectorAll('a[data-is-diff-selected="true"]');
+      const selectedNode = getSelectedNode();
       if (selectedNode.length === 2) {
-        selectedNode.forEach((el) => {
-          el.style['background-color'] = '';
-          el.dataset.isDiffSelected = false;
-        });
-        /* 色変えたやつとかdataをリセットして新規タブ開く */
-        let repositoryUrl = document.querySelector('.repohead-details-container > h1 > strong > a').href;
-        document.querySelector('html').onclick = null;
-        window.open(`${repositoryUrl}/compare/${selectedNode[0].innerText}...${selectedNode[1].innerText}`);
-      }
+        /* 新規タブ開く */
+        if (isActiveCompare()) window.open(buildCompareUrl(selectedNode[0], selectedNode[1]));
 
-      /* 遷移を止めるため */
-      return false;
+        /* リセット */
+        removeEvent();
+        selectedNode.forEach(unselectCommit)
+      }
     }
+    /* 遷移を止めるため */
+    return false;
+  }
+  function getSelectedNode() {
+    return document.querySelectorAll('a[data-is-diff-selected="true"]');
+  }
+  function selectCommit(el) {
+    el.dataset.isDiffSelected = true;
+    el.style['background-color'] = '#404040';
+  }
+  function unselectCommit(el) {
+    el.dataset.isDiffSelected = false;
+    el.style['background-color'] = '';
+  }
+  function isCommitLink(el) {
+    return el.classList.contains('commit-id')
+  }
+  function buildCompareUrl(startCommitEl, endCommitEl) {
+    const repositoryUrl = document.querySelector('.repohead-details-container > h1 > strong > a').href;
+    return `${repositoryUrl}/compare/${startCommitEl.innerText}...${endCommitEl.innerText}`;
   }
 })()
